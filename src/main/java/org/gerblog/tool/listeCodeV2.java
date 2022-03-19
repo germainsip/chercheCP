19fde0924de94f424ec7ace7f4277380c994c048
Germain SIPIERE <gsipiere@gmail.com>
2022-03-19 12:07:44 +0100

    listeCodeV2 in progress

--- src/main/java/org/gerblog/tool/listeCodeV2.java
+++ src/main/java/org/gerblog/tool/listeCodeV2.java
@@ -1,6 +1,9 @@
 package org.gerblog.tool;
 
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 import com.opencsv.exceptions.CsvException;
 
 import java.io.*;
@@ -15,8 +18,9 @@ public class listeCodeV2 {
     public listeCodeV2() {
         InputStream is = getClass().getClassLoader().getResourceAsStream("sourceCSV/laposte.csv");
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        CSVParser csvParser = new CSVParserBuilder().withSeparator(';').build();
         //File csv = new File(getClass().getResourceAsStream("/sourceCSV/laposte.csv"));
-        try (CSVReader reader = new CSVReader(br)) {
+        try (CSVReader reader = new CSVReaderBuilder(br).withCSVParser(csvParser).withSkipLines(1).build()) {
             this.listeDuCSV = reader.readAll();
            // r.forEach(x -> System.out.println(Arrays.toString(x)));
         } catch (IOException e) {
@@ -29,11 +33,13 @@ public class listeCodeV2 {
     public List<String> cpSearch(){
         List<String> list = new ArrayList<>();
         listeDuCSV.forEach(x -> {
-            System.out.println(x.length);
-
+            if(x[1].equals("AMIENS"))
+            {list.add(x[2]);
+            System.out.println(x[1]);}
         });
-       // list.add(listeDuCSV.get(0)[0]);
+        //list.add(listeDuCSV.get(0)[0]);
 
+       // System.out.println(listeDuCSV.get(0));
 
         return list;
     }
